08bd906ee46926838dd4b0e3bae8ab153cf56f94
Maria-Cristina Ciocci <mcristina.ciocci@gmail.com>
2018-06-01 19:53:33 +0200

    on reply button press, store info so the chatbot can do the reply

--- src/config.py
+++ src/config.py
@@ -14,3 +14,11 @@ UserName = "YOUR_GMAIL_ACCOUNT@gmail.com"
 UserPassword = "YOUR_GMAIL_PASSWORD"
 SmptPort = 587  # google: 465
 SmptServer = 'smtp.gmail.com' #localhost' # google: 'smtp.gmail.com'
+
+# is a reply button connected to the TVbox ?
+REPLY_BUTTON = False
+REPLY_PIN = 18
+NEXT_BUTTON = False
+NEXT_PIN = 17
+PREV_BUTTON = False
+PREV_PIN = 23
