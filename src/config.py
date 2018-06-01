65314471a3a3d60493377a178a3bf42ea746f158
Maria-Cristina Ciocci <mcristina.ciocci@gmail.com>
2018-06-01 19:20:38 +0200

    introduce a config file for all configuration

---
+++ src/config.py
@@ -0,0 +1,16 @@
+
+# the token of your bot obtained from Telegram
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"
+
+#send email of pictures you receive?
+SEND_EMAIL_OF_PICS = False
+
+#email of photoframe to send pics to
+EMAIL_FRAME = "YOUR_PIXSTAR_EMAIL@mypixstar.com"
+
+#to send mail we need to use smtp of google. Give here sending mail info 
+# note: required install: sudo apt-get install ssmtp 
+UserName = "YOUR_GMAIL_ACCOUNT@gmail.com"
+UserPassword = "YOUR_GMAIL_PASSWORD"
+SmptPort = 587  # google: 465
+SmptServer = 'smtp.gmail.com' #localhost' # google: 'smtp.gmail.com'
